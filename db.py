54b2bd457d4e19bcb2cec73fde8dd7b999780731
bod3 <78141647+bodejan@users.noreply.github.com>
2024-05-27 12:06:54 +0200

    update db description

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, Float, String, Text, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, Float, VARCHAR, Text, ForeignKey, VARCHAR
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
@@ -9,89 +9,135 @@ Base = declarative_base()
 
 class MedicalRecord(Base):
     __tablename__ = 'medical_records'
+    __table_args__ = {
+        'comment': "Table containing This data is from the California Department of Managed Health Care (DMHC). It contains all decisions from Independent Medical Reviews (IMR) administered by the DMHC since January 1, 2001. An IMR is an independent review of a denied, delayed, or modified health care service that the health plan has determined to be not medically necessary, experimental/investigational or non-emergent/urgent. If the IMR is decided in an enrollee's favor, the health plan must authorize the service or treatment requested."}
 
-    reference_id = Column(String, primary_key=True)
-    report_year = Column(Integer)
-    diagnosis_category = Column(String)
-    diagnosis_sub_category = Column(String)
-    treatment_category = Column(String)
-    treatment_sub_category = Column(String)
-    determination = Column(String)
-    treatment_type = Column(String)
-    patient_age_range = Column(String)
-    patient_gender = Column(Integer)
-    findings = Column(Text)
-    patient_name = Column(String)
-    patient_age = Column(Float)
-    patient_phone = Column(String)
-    patient_address = Column(String)
-    patient_blood_type = Column(String)
-    patient_ssn = Column(String)
-    patient_insurance_provider = Column(String)
-    patient_insurance_number = Column(String)
-    patient_emergency_contact = Column(String)
-    patient_occupation = Column(String)
-    consulting_physician = Column(String)
+    reference_id = Column(VARCHAR, primary_key=True,
+                          comment='Unique reference ID for the medical record')
+    report_year = Column(
+        Integer, comment='Year of the medical report, available options: [2016 2006 2015 2014 2010 2005 2004 2009 2008 2007 2001 2013 2012 2002 2003 2011]')
+    diagnosis_category = Column(VARCHAR, comment="Primary diagnosis category, available options: ['Infectious' 'Mental' 'Autism Spectrum' 'Prevention/Good Health' 'Cardiac/Circulatory' 'OB-Gyn / Pregnancy' 'Digestive System / Gastrointestinal' 'Orthopedic / Musculoskeletal' 'Central Nervous System/ Neuromuscular' 'Endocrine/ Metabolic' 'Pediatrics' 'Chronic Pain' 'Respiratory System' 'Cancer' 'Morbid Obesity' 'Ears, Nose, Throat' 'Post Surgical Complication' 'Immunologic' 'Skin' 'Not Applicable' 'Foot' 'Dental' 'Blood Related' 'Genetic' 'Genitourinary/ Kidney' 'Vision' 'Trauma/Injuries' 'Organ Failure' 'Alcohol and Drug Addiction']")
+    diagnosis_sub_category = Column(
+        VARCHAR, comment='Sub-category of the diagnosis')
+    treatment_category = Column(VARCHAR, comment="Primary treatment category, available options: ['Pharmacy/Prescription Drugs' 'Mental Health Treatment' 'Autism Related Treatment' 'Diagnostic Imaging, Screening and Testing' 'Cardio Vascular' 'Durable Medical Equipment' 'Diagnostic/Physician Evaluation' 'Orthopedic' 'Emergency/Urgent Care' 'General Surgery' 'Acute Medical Services - Outpatient' 'Not Applicable' 'Pain Management' 'Cancer Treatment' 'Reconstructive/Plastic Surgery' 'Rehabilitation Services - Skilled Nursing Facility - Inpatient' 'Special Procedure' 'Electrical/ Thermal/ Radiofreq. Interventions' 'Alternative Treatment' 'OB/GYN Procedures' 'Neurosugery' 'Dental/Orthodontic' 'Home Health Care' 'Acute Medical Services - Inpatient' 'Ear, Nose and Throat Procedures' 'Rehabilitation Services - Outpatient' 'Vision' 'Urology' nan 'Preventive Health Screening' 'Chiropractic' 'Ophthalmology']")
+    treatment_sub_category = Column(
+        VARCHAR, comment='Sub-category of the treatment')
+    determination = Column(
+        VARCHAR, comment="Indicates if the determination was upheld or overturned, available options: ['Overturned Decision of Health Plan' 'Upheld Decision of Health Plan']")
+    treatment_type = Column(
+        VARCHAR, comment="Type of treatment administered, available options: ['Medical Necessity' 'Experimental/Investigational' 'Urgent Care']")
+    patient_age_range = Column(
+        VARCHAR, comment="Age range of the patient, available options: ['41-50' '21-30' '0-10' '65+' '51-64' '11_20' '31-40' nan]")
+    patient_gender = Column(
+        Integer, comment="Gender of the patient, available options: ['Male' 'Female' nan]")
+    findings = Column(Text, comment='Findings from the medical examination')
+    patient_name = Column(VARCHAR, comment='Full name of the patient')
+    patient_age = Column(Float, comment='Age of the patient')
+    patient_phone = Column(VARCHAR, comment='Phone number of the patient')
+    patient_address = Column(VARCHAR, comment='Address of the patient')
+    patient_blood_type = Column(
+        VARCHAR, comment="Blood type of the patient, available options: ['A-' 'B+' 'O+' 'AB-' 'O-' 'AB+' 'B-' 'A+']")
+    patient_ssn = Column(
+        VARCHAR, comment='Social Security Number of the patient')
+    patient_insurance_provider = Column(
+        VARCHAR, comment="Insurance provider of the patient, available options: ['Kaiser Foundation' 'Highmark' 'Anthem Inc.' 'Cigna Health' 'HCSC(Health Care Service Corporation)' 'Molina Healthcare' 'WellCare' 'Medicare' 'Humana' 'UnitedHealth Group' 'Independence Health Group' 'Medicaid' 'CVS Health(Aetna)' 'Blue Cross Blue Shield' 'Centene Corporation' 'GuideWell Mutual Holding']")
+    patient_insurance_number = Column(
+        VARCHAR, comment='Insurance number of the patient')
+    patient_emergency_contact = Column(
+        VARCHAR, comment='Emergency contact details of the patient')
+    patient_occupation = Column(VARCHAR, comment='Occupation of the patient')
+    consulting_physician = Column(
+        VARCHAR, comment="Physician who consulted on the case, available options: ['Dr. Jerry Daniels' 'Dr. Eddie Young' 'Dr. Michelle Lamb' 'Dr. Shelly Hunt' 'Dr. Alexandria Gaines' 'Dr. James Barber']")
 
-    metadata_ = relationship(
-        "MedicalMetadata", back_populates="medical_record", uselist=False)
+    metadata_ = relationship("MedicalMetadata", back_populates="medical_record",
+                             uselist=False)
 
 
 class MedicalMetadata(Base):
     __tablename__ = 'medical_metadata'
+    __table_args__ = {
+        'comment': 'Table containing purpose-based access control metadata for medical records; allowed intended purposes (AIP) and prohibited intended purposes (PIP)'}
 
-    reference_id = Column(String, ForeignKey(
-        'medical_records.reference_id'), primary_key=True)
-    diagnosis_category_aip = Column(Integer)
-    diagnosis_category_pip = Column(Integer)
-    diagnosis_sub_category_aip = Column(Integer)
-    diagnosis_sub_category_pip = Column(Integer)
-    treatment_category_aip = Column(Integer)
-    treatment_category_pip = Column(Integer)
-    treatment_sub_category_aip = Column(Integer)
-    treatment_sub_category_pip = Column(Integer)
-    determination_aip = Column(Integer)
-    determination_pip = Column(Integer)
-    treatment_type_aip = Column(Integer)
-    treatment_type_pip = Column(Integer)
-    patient_age_range_aip = Column(Integer)
-    patient_age_range_pip = Column(Integer)
-    findings_aip = Column(Integer)
-    findings_pip = Column(Integer)
-    patient_name_aip = Column(Integer)
-    patient_name_pip = Column(Integer)
-    patient_age_aip = Column(Integer)
-    patient_age_pip = Column(Integer)
-    patient_phone_aip = Column(Integer)
-    patient_phone_pip = Column(Integer)
-    patient_address_aip = Column(Integer)
-    patient_address_pip = Column(Integer)
-    patient_blood_type_aip = Column(Integer)
-    patient_blood_type_pip = Column(Integer)
-    patient_ssn_aip = Column(Integer)
-    patient_ssn_pip = Column(Integer)
-    patient_insurance_provider_aip = Column(Integer)
-    patient_insurance_provider_pip = Column(Integer)
-    patient_insurance_number_aip = Column(Integer)
-    patient_insurance_number_pip = Column(Integer)
-    patient_emergency_contact_aip = Column(Integer)
-    patient_emergency_contact_pip = Column(Integer)
-    patient_occupation_aip = Column(Integer)
-    patient_occupation_pip = Column(Integer)
-    consulting_physician_aip = Column(Integer)
-    consulting_physician_pip = Column(Integer)
+    reference_id = Column(VARCHAR, ForeignKey('medical_records.reference_id'),
+                          primary_key=True, comment='Reference ID linking to the medical record')
+    diagnosis_category_aip = Column(
+        Integer, comment='Diagnosis category AIP code')
+    diagnosis_category_pip = Column(
+        Integer, comment='Diagnosis category PIP code')
+    diagnosis_sub_category_aip = Column(
+        Integer, comment='Diagnosis sub-category AIP code')
+    diagnosis_sub_category_pip = Column(
+        Integer, comment='Diagnosis sub-category PIP code')
+    treatment_category_aip = Column(
+        Integer, comment='Treatment category AIP code')
+    treatment_category_pip = Column(
+        Integer, comment='Treatment category PIP code')
+    treatment_sub_category_aip = Column(
+        Integer, comment='Treatment sub-category AIP code')
+    treatment_sub_category_pip = Column(
+        Integer, comment='Treatment sub-category PIP code')
+    determination_aip = Column(Integer, comment='Determination AIP code')
+    determination_pip = Column(Integer, comment='Determination PIP code')
+    treatment_type_aip = Column(Integer, comment='Treatment type AIP code')
+    treatment_type_pip = Column(Integer, comment='Treatment type PIP code')
+    patient_age_range_aip = Column(
+        Integer, comment='Patient age range AIP code')
+    patient_age_range_pip = Column(
+        Integer, comment='Patient age range PIP code')
+    findings_aip = Column(Integer, comment='Findings AIP code')
+    findings_pip = Column(Integer, comment='Findings PIP code')
+    patient_name_aip = Column(Integer, comment='Patient name AIP code')
+    patient_name_pip = Column(Integer, comment='Patient name PIP code')
+    patient_age_aip = Column(Integer, comment='Patient age AIP code')
+    patient_age_pip = Column(Integer, comment='Patient age PIP code')
+    patient_phone_aip = Column(Integer, comment='Patient phone AIP code')
+    patient_phone_pip = Column(Integer, comment='Patient phone PIP code')
+    patient_address_aip = Column(Integer, comment='Patient address AIP code')
+    patient_address_pip = Column(Integer, comment='Patient address PIP code')
+    patient_blood_type_aip = Column(
+        Integer, comment='Patient blood type AIP code')
+    patient_blood_type_pip = Column(
+        Integer, comment='Patient blood type PIP code')
+    patient_ssn_aip = Column(Integer, comment='Patient SSN AIP code')
+    patient_ssn_pip = Column(Integer, comment='Patient SSN PIP code')
+    patient_insurance_provider_aip = Column(
+        Integer, comment='Patient insurance provider AIP code')
+    patient_insurance_provider_pip = Column(
+        Integer, comment='Patient insurance provider PIP code')
+    patient_insurance_number_aip = Column(
+        Integer, comment='Patient insurance number AIP code')
+    patient_insurance_number_pip = Column(
+        Integer, comment='Patient insurance number PIP code')
+    patient_emergency_contact_aip = Column(
+        Integer, comment='Patient emergency contact AIP code')
+    patient_emergency_contact_pip = Column(
+        Integer, comment='Patient emergency contact PIP code')
+    patient_occupation_aip = Column(
+        Integer, comment='Patient occupation AIP code')
+    patient_occupation_pip = Column(
+        Integer, comment='Patient occupation PIP code')
+    consulting_physician_aip = Column(
+        Integer, comment='Consulting physician AIP code')
+    consulting_physician_pip = Column(
+        Integer, comment='Consulting physician PIP code')
 
-    medical_record = relationship("MedicalRecord", back_populates="metadata_")
+    medical_record = relationship(
+        "MedicalRecord", back_populates="metadata_")
 
 
 class Purpose(Base):
     __tablename__ = 'purposes'
+    __table_args__ = {
+        'comment': 'Table containing purpose codes for medical records'}
 
-    id = Column(Integer, primary_key=True)
-    parent_id = Column(Integer, ForeignKey('purposes.id'), nullable=True)
-    code = Column(Integer)
-    aip_code = Column(Integer)
-    pip_code = Column(Integer)
+    id = Column(Integer, primary_key=True, comment='Primary key')
+    name = Column(VARCHAR, comment='Name of the purpose')
+    parent_id = Column(Integer, ForeignKey('purposes.id'), nullable=True,
+                       comment='Parent purpose ID for hierarchical purposes')
+    code = Column(Integer, comment='Purpose code')
+    aip_code = Column(Integer, comment='AIP code for the purpose')
+    pip_code = Column(Integer, comment='PIP code for the purpose')
+    description = Column(VARCHAR, comment='Description of the purpose')
 
     parent = relationship("Purpose", remote_side=[id], backref="children")
 
@@ -113,10 +159,12 @@ def populate_db():
     for index, row in purposes_data.iterrows():
         purpose = Purpose(
             id=row['id'],
+            name=row['name'],
             parent_id=row['parent_id'],
             code=row['code'],
             aip_code=row['aip_code'],
-            pip_code=row['pip_code']
+            pip_code=row['pip_code'],
+            description=row['description']
         )
         session.add(purpose)
 
--- db_utils.py
+++ db_utils.py
@@ -38,39 +38,55 @@ def print_all_purposes(session):
         print(purpose.id, purpose.code, purpose.aip_code, purpose.pip_code)
 
 
+def get_schema_info(base):
+    schema_info = {}
+    for cls in base.__subclasses__():
+        # Get the table comment
+        table_description = cls.__table__.comment if cls.__table__.comment else ''
+
+        columns_info = {}
+        for column in cls.__table__.columns:
+            # Get the column comment
+            column_description = column.comment if column.comment else ''
+            columns_info[column.name] = {
+                'type': str(column.type),
+                'description': column_description
+            }
+
+        relationships_info = {}
+        for rel in cls.__mapper__.relationships:
+            # Get the relationship comment from the info dictionary
+            rel_description = rel.info.get('description', '')
+            relationships_info[rel.key] = {
+                'description': rel_description
+            }
+
+        table_info = {
+            'description': table_description,
+            'columns': columns_info,
+            'relationships': relationships_info
+        }
+        schema_info[cls.__tablename__] = table_info
+
+    # Format the schema information as a structured string
+    structured_string = ""
+    for table, info in schema_info.items():
+        structured_string += f"Table: {table}\n"
+        structured_string += f"Description: {info['description']}\n"
+        structured_string += "Columns:\n"
+        for column_name, column_info in info['columns'].items():
+            structured_string += f"  - {column_name} ({column_info['type']}): {
+                column_info['description']}\n"
+        structured_string += "Relationships:\n"
+        for rel_name, rel_info in info['relationships'].items():
+            structured_string += f"  - {rel_name}: {rel_info['description']}\n"
+        structured_string += "\n"
+
+    return structured_string
+
+
 def close_session(session):
     session.close()
-
-
-def get_schema_with_comments(engine):
-    inspector = inspect(engine)
-    schema = []
-
-    for table_name in inspector.get_table_names():
-        schema.append(f"Table: {table_name}")
-
-        # Get the table comment
-        table_comment = inspector.get_table_comment(table_name).get('text', '')
-        if table_comment:
-            schema.append(f"  Comment: {table_comment}")
-
-        # Get columns information
-        columns = inspector.get_columns(table_name)
-        for column in columns:
-            column_name = column['name']
-            column_type = column['type']
-            column_comment = column.get('comment', '')
-            column_primary_key = 'Primary Key' if column.get(
-                'primary_key', False) else ''
-            column_info = f"  Column: {column_name} ({column_type}) {
-                column_primary_key}"
-            if column_comment:
-                column_info += f" - {column_comment}"
-            schema.append(column_info)
-
-        schema.append("")  # Add an empty line for better readability
-
-    return "\n".join(schema)
 
 
 if __name__ == "__main__":
@@ -78,7 +94,7 @@ if __name__ == "__main__":
     session = get_session()
 
     try:
-        # Example usage
+        """ # Example usage
         reference_id = 'MN16-22639'
 
         medical_record = get_medical_record_by_reference_id(
@@ -92,7 +108,8 @@ if __name__ == "__main__":
         print_all_purposes(session)
 
         print("All Medical Records:")
-        # print_all_medical_records(session)
+        print_all_medical_records(session) """
+        print(get_schema_info(Base))
     finally:
         # Close the session
         close_session(session)
